a6a2de02cf804aa3bd529640213875f135ee77e7
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2016-06-02 18:20:32 +0100

    Initial parsing of the files.

--- examples/simple.js
+++ examples/simple.js
@@ -1,3 +1,3 @@
 // if you don't specify a html file, the sniper will generate a div with id "rootDiv"
 var app = require("biojs-vis-chromosome-heatmap");
-var instance = new app({el: rootDiv, text: 'biojs'});
+var instance = new app.ChromosomeHeatmap({el: rootDiv, text: 'biojs', data:'heatmapsCadenza/cadenza.json'});
