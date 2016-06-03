b56d455b053edae10de11d746fb48468c3a62a6f
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2016-06-03 18:08:16 +0100

    First plot that displays all the data

--- examples/simple.js
+++ examples/simple.js
@@ -1,3 +1,6 @@
 // if you don't specify a html file, the sniper will generate a div with id "rootDiv"
 var app = require("biojs-vis-chromosome-heatmap");
-var instance = new app.ChromosomeHeatmap({el: rootDiv, text: 'biojs', data:'heatmapsCadenza/cadenza.json'});
+var instance = new app.ChromosomeHeatmap({el: rootDiv, 
+	//width:500,
+	//height:700, 
+	data:'heatmapsCadenza/cadenza.json'});
